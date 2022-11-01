c139366738a44c3effc1ccf3a2dda9d266d5d39a
zxxta <117131842+zxxta@users.noreply.github.com>
2022-11-01 08:36:16 +0700

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -15,24 +15,24 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
 global.youtube = 'https://youtube.com/c/HwModsWa857'
 global.ig = 'https://www.instagram.com/hikal_857/?hl=id'
-global.mygc = 'wa.me/6285714170944'
+global.mygc = 'wa.me/6281770447981'
 global.myweb = 'https://eclass.iainsalatiga.ac.id/app/upload/users/1/10892/my_files/XhiroMhonshine.html'
 global.linkgrupss = "https://chat.whatsapp.com/Jh7zaCSnPCBC4gdYOEVm0K"
 global.linkpro = 'mailto:support@whatsapp.com?subject=Deactivate%20My%20Account%20Number&body=Please%20Deactivate%20The%20My%20Account%20Number%20%2B62XXXXXXXX%20Immediately%20Because%20The%20Number%20Has%20Been%20Lost'
-global.email = 'aldimoder2@gmail.com'
+global.email = 'zidannmaulaa@gmail.com'
 global.region = 'indonesia'
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'AldSLEBEWW' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'AldSLEBEWW' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'AldSLEBEWW' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Zidann' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Zidann' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Zidann' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6285697175057']
-global.premium = ['6285697175057']
-global.packname = 'AldSLEBEWW'
-global.ownerr = ['AldSLEBEWW']
-global.author = 'AldSLEBEWW'
-global.sessionName = './sessionye/haikal'
+global.owner = ['6281770447981']
+global.premium = ['6281770447981']
+global.packname = 'Zidann'
+global.ownerr = ['Zidann']
+global.author = 'Zidann'
+global.sessionName = './sessionye/maker'
 global.prefa = ['','!','.',',','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
@@ -41,7 +41,7 @@ global.mess = {
     private: 'Fitur Digunakan Hanya Untuk Private Chat!',
     bot: 'Fitur Khusus Pengguna Nomor Bot',
     endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
-    verif: 'Hai Kakak 👋 Silahkan Verifikasi Terlebih Dahulu Agar Bisa Menggunakan Bot Hw Mods',
+    verif: 'Hai Kakak Silahkan Verifikasi Terlebih Dahulu Agar Bisa Menggunakan Bot',
 }
 //=================================================//
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
